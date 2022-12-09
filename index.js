be7eea1218b3cc4db2dc384f6dfa697b71c58dda
BorisDundakov <boris.stefan.de@gmail.com>
2022-12-09 12:21:11 +0200

    PAT atempt 1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const core = require('@actions/core')
-// const github = require('@actions/github')
-
 async function run(){
     console.log('inside index.js file')
+    const myToken = core.getInput('myToken');
+    const octokit = github.getOctokit(myToken)
+    const context = github.context;
 
 }
 
