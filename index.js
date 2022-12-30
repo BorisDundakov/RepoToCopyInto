8d83f639c2eeb2b604ebfd41006c8642ed678b8d
Boris Dundakov <71731579+BorisDundakov@users.noreply.github.com>
2022-12-31 00:38:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,30 @@ const email = 'boris.stefan.de@gmail.com'
 
 
 async function update_readme(){
+    //console.log('inside index.js file')
     const myToken = core.getInput('myToken');
     const octokit = github.getOctokit(myToken)
     const context = github.context;
 
-    const { data: {sha}} = await octokit.request('GET /repos/{owner}/{repo}/contents/README.md/', {
-        owner: context.repo.owner,
-        repo: context.repo.repo,'
+    // connect to current repo via Octokit
+    const { data: {sha}} = await octokit.request('GET /repos/BorisDundakov/RepoToCopyInto/contents/README.md/', {
+        owner: 'bosch-ecs-career-camp',
+        repo: 'homework4-BorisDundakov'
       })
-
       
-      const { data: {content}} = await octokit.request('GET /repos/{owner}/{repo}/contents/README.md/', {
+     //console.log(sha)
+      
+      const { data: {content}} = await octokit.request('GET /repos/BorisDundakov/RepoToCopyFrom/contents/README.md/', {
         owner: 'BorisDundakov',
         repo: 'RepoToCopyFrom'
       })
       
+      //console.log(content)
       
       const update = await octokit.request('PUT /repos/{owner}/{repo}/contents/README.md', {
                             owner: context.repo.owner,
                             repo: context.repo.repo,
-                            //path: 'README.md',
+                            path: 'README.md',
                             message: 'replace current README with new README from another repo',
                             commiter: {
                               name: 'Boris Dundakov',
